7eb3fe1beb94b085c02dc92f41a1d03e9c271ac7
zukaaman <zukaaman@gmail.com>
2017-11-10 18:36:00 +0400

    почти готово

--- src/js/partials/actions-open.js
+++ src/js/partials/actions-open.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var actionsButton = document.querySelector(".actions__open-button");
   var actionsText = document.querySelector(".actions__text");
+  var shareButton = document.querySelector(".actions__share-button");
+  var shareList = document.querySelector(".actions__share");
 
   actionsButton.addEventListener("click", function() {
     actionsText.classList.toggle("actions__text--closed");
     actionsButton.classList.toggle("actions__open-button--open");
+  });
+
+  shareButton.addEventListener("click", function() {
+    shareList.classList.toggle("actions__share--open");
   });
 });
