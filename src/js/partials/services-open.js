766fb2cf8ac9ffb1cba24de5ac442c895b519e51
zukaaman <zukaaman@gmail.com>
2017-11-14 18:32:12 +0400

    все готово кроме модалок, косметики и адаптива

--- src/js/partials/services-open.js
+++ src/js/partials/services-open.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var servicesButtonSale = document.querySelector(".main__button-order--sale");
   var servicesForm = document.querySelector(".main__form");
   var servicesButtonClose = document.querySelector(".main__button-close");
+  var servicesModelOther = document.querySelector(".model-other");
+  var servicesModelInput = document.querySelector(".model-list--other-input");
+  var servicesModelClose = document.querySelector(".model-other__button-close");
+  var servicesDefectInput = document.querySelector(".defect-other-input");
+  var servicesDefectOther = document.querySelector(".defect-other");
+  var servicesDefectClose = document.querySelector(".defect-other__button-close");
+  var servicesCommentButton = document.querySelector(".main__form-button-comment");
+  var servicesCommentOther = document.querySelector(".comment-other");
+  var servicesCommentClose = document.querySelector(".comment-other__button-close");
+  var servicesDefectModernInput = document.querySelector(".defect-modern-other-input");
+  var servicesDefectModernOther = document.querySelector(".defect-modern-other");
+  var servicesDefectModernClose = document.querySelector(".defect-modern-other__button-close");
+
 
   servicesButtonRepair.addEventListener("click", function() {
     servicesForm.classList.remove("main__form--closed");
@@ -29,5 +42,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
     servicesButtonRepair.classList.add("main__button-order--closed");
     servicesButtonModern.classList.add("main__button-order--closed");
     servicesButtonSale.classList.add("main__button-order--closed");
-  })
+  });
+////////////////////////////////////////////////////////////
+  servicesModelInput.addEventListener("focus", function() {
+    servicesModelOther.classList.remove("model-other--closed");
+  });
+
+  servicesModelClose.addEventListener("click", function() {
+    servicesModelOther.classList.add("model-other--closed");
+  });
+
+  servicesDefectInput.addEventListener("focus", function() {
+    servicesDefectOther.classList.remove("defect-other--closed");
+  });
+
+  servicesDefectClose.addEventListener("click", function() {
+    servicesDefectOther.classList.add("defect-other--closed");
+  });
+
+  servicesCommentButton.addEventListener("click", function() {
+    servicesCommentOther.classList.remove("comment-other--closed");
+  });
+
+  servicesCommentClose.addEventListener("click", function() {
+    servicesCommentOther.classList.add("comment-other--closed");
+  });
+
+  servicesDefectModernInput.addEventListener("focus", function() {
+    servicesDefectModernOther.classList.remove("defect-modern-other--closed");
+  });
+
+  servicesDefectModernClose.addEventListener("click", function() {
+    servicesDefectModernOther.classList.add("defect-modern-other--closed");
+  });
 });
